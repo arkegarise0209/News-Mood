feda90149c7b59a2a12a5190cfe11552f89c4816
Kegarise <kegarise@queensgranthigh.org>
2018-03-30 00:20:30 -0400

    start

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "vmhIZsI1CmObPW4fWPj5kkgCP"
+consumer_secret = "XOkdl2rBOcF5FXXIF6NMJ7ZbqJhCYNzcGEZtqCrc8EdG2BCn9U"
+access_token = "977545693324816384-vuHJIHwc4ZgwzPTKReEOnpkvyWOv3Ol"
+access_token_secret = "B6BAvEolfp5m8l11wi5smZW6eNN3zRWB1MiQPaAcuhnCf"
